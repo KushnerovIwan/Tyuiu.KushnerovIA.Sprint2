626731cb1fcda1a133c2b69b5d23bd82b21143be
tyuiu.KushnerovIA@outlook.com <tyuiu.KushnerovIA@outlook.com>
2023-10-19 22:08:42 +0500

    Add project files.

---
+++ Tyuiu.KushnerovIA.Sprint2.Task0.V22.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.KushnerovIA.Sprint2.Task0.V22.Lib;
+
+namespace Tyuiu.KushnerovIA.Sprint2.Task0.V22.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpressoin()
+        {
+            DataService ds = new DataService();
+            int x = 3105;
+            int y = 77;
+            bool[] res = ds.GetCompareOperations(x, y);
+            bool[] prov = { false, true, true, true, true, false };
+            CollectionAssert.AreEqual(prov, res);
+        }
+    }
+}
---
+++ Tyuiu.KushnerovIA.Sprint2.Task0.V22.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KushnerovIA.Sprint2.Task0.V22.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("Tyuiu.KushnerovIA.Sprint2.Task0.V22.Test")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("84e219af-7837-4b8f-b260-8ffc1b2ea737")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
