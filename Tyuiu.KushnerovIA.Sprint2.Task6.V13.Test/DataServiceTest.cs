c7cb27853f6d7d15607a640925d7d8b7d072a88e
tyuiu.KushnerovIA@outlook.com <tyuiu.KushnerovIA@outlook.com>
2023-10-20 19:05:28 +0500

    Task6

---
+++ Tyuiu.KushnerovIA.Sprint2.Task6.V13.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.KushnerovIA.Sprint2.Task6.V13.Lib;
+
+namespace Tyuiu.KushnerovIA.Sprint2.Task6.V13.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidFindDateOfNextDay()
+        {
+            DataService ds = new DataService();
+            string res = "2022.12.2";
+            int g = 2022;
+            int m = 12;
+            int n = 03;
+            Assert.AreEqual(res, ds.FindDateOfNextDay(g, m, n));
+        }
+    }
+}
---
+++ Tyuiu.KushnerovIA.Sprint2.Task6.V13/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KushnerovIA.Sprint2.Task6.V13.Lib;
+
+namespace Tyuiu.KushnerovIA.Sprint2.Task6.V13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
